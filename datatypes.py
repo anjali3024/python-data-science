db0e5856f2ba96fe2ccbde83e9a95f2ea967f321
anjali3024 <anjalisingh09q@gmail.com>
2022-07-31 01:36:33 -0700

    basics

---
+++ datatypes.py
@@ -0,0 +1,34 @@
+
+
+x = 1920
+y = 9203
+x2 = -39021309
+y2 = -68327646487
+print(x,type(x))
+print(y,type(y))
+print(x2,type(x2))
+print(y2,type(y2))
+z = 1.01
+z1 = 2.014e200
+print(z,type(z))
+print(z1,type(z1))
+p = 'sam'
+q = 'vinay'
+u = "shivangi"
+print(p, type(p))
+print(q, type(q))
+print(u, type(u))
+t = None
+print(t,type(t))
+a = [5,10,15,20,25]
+vals = [1,2.2,'python']
+colors = ['red','green','yellow']
+print(a,type(a))
+print(vals,type(vals))
+print(colors,type(colors))
+car_info = {
+    "brand": "ford",
+    "model": "mustang",
+    "year": "1964"
+    }
+print(car_info) 
---
+++ opeors.py
@@ -0,0 +1,11 @@
+a = 10
+b = 3
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+fruits = ['Apple',"banana", "orange"]
+print("papaya" in fruits)
+print("Apple" in fruits)
